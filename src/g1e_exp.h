55c79aee8fce8f4f0e6fa7bc5102ee5609ebd7ac
Tri Le <tri.h.le@colorado.edu>
2020-03-02 11:18:34 -0700

    Update g1e_exp.h

--- src/g1e_exp.h
+++ src/g1e_exp.h
@@ -23,10 +23,5 @@
 #include <complex.h>
 
 void CINTg_nuc_exp(double *g, double* rij, double* cr, double aij, double ak, double fac, CINTEnvVars *envs);
-void CINTg_nuc_exp_ksum(double *g, double* rij, double* cr, double aij, double ak, double fac, CINTEnvVars *envs);
 FINT num_kterms(double ak, double wk, double epsilion = 1E-10);
-double JTsum(double* gk_real, double* gk_img, double* Ri, double* Rj, double* Rc, double* alpha, FINT Nk, FINT D);
-double JTsum_x0(double* gk_real, double* gk_img, double* R, double* alpha, FINT Nk);
-double JTsum_x1(double* gk_real, double* gk_img, double* R, double* alpha, FINT Nk);
-double JTsum_x2(double* gk_real, double* gk_img, double* R, double* alpha, FINT Nk);
 #endif /* g1e_exp_h */
