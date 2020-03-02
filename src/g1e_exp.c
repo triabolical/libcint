bba1c11694aacf8056e87032dd6e3b146afec3f6
Tri Le <tri.h.le@colorado.edu>
2020-03-02 11:19:22 -0700

    Update g1e_exp.c

--- src/g1e_exp.c
+++ src/g1e_exp.c
@@ -9,59 +9,32 @@
 #include "g1e_exp.h"
 
 void CINTg_nuc_exp(double *g, double* rij, double* cr, double aij, double ak, double fac, CINTEnvVars *envs){
-        const FINT nmax = envs->li_ceil + envs->lj_ceil;
-        const FINT lj = envs->lj_ceil;
-        const FINT dj = envs->g_stride_j;
-        const double *ri = envs->ri;
-        const double *rj = envs->rj;
-        FINT i, j, ptr;
-        double ririj[3], rirj[3];
-        double *gx = g;
-        double *gy = g + envs->g_size;
-        double *gz = g + envs->g_size * 2;
-        double pij[3];
-        double aijk = aij + ak;
-        double rijrk = 0, dr;
-        for (FINT k = 0; k < 3; ++k){
-            rirj[k] = ri[k] - rj[k];
-            pij[k] = ( aij*rij[k] + ak*cr[k] ) / aijk;
-            ririj[k] = ri[0] - pij[k];
-            dr = rij[k] - cr[k];
-            rijrk += dr*dr;
-        }
-        double fac2 = exp(-(aij*ak/ aijk)*rijrk)*fac;
-        gx[0] = 1;
-        gy[0] = 1;
-        gz[0] = SQRTPI * PI * fac2;
-        if (nmax > 0) {
-                gx[1] = -ririj[0] * gx[0];
-                gy[1] = -ririj[1] * gy[0];
-                gz[1] = -ririj[2] * gz[0];
-        }
-
-        for (i = 1; i < nmax; i++) {
-                gx[i+1] = 0.5 * i / aij * gx[i-1] - ririj[0] * gx[i];
-                gy[i+1] = 0.5 * i / aij * gy[i-1] - ririj[1] * gy[i];
-                gz[i+1] = 0.5 * i / aij * gz[i-1] - ririj[2] * gz[i];
-        }
- 
-        for (j = 1; j <= lj; j++) {
-                ptr = dj * j;
-                for (i = ptr; i <= ptr + nmax - j; i++) {
-                        gx[i] = gx[i+1-dj] + rirj[0] * gx[i-dj];
-                        gy[i] = gy[i+1-dj] + rirj[1] * gy[i-dj];
-                        gz[i] = gz[i+1-dj] + rirj[2] * gz[i-dj];
-                }
-        }
-}
-void CINTg_nuc_exp_ksum(double *g, double* rij, double* cr, double aij, double ak, double fac, CINTEnvVars *envs){
+    
+    /*
+     rij = ( ai*ri + aj*rj ) / aij
+     cr = Rc nuc position
+     fac = exp(-ai*aj/aij*Rij^2) * pk
+     */
+    
+    FINT gsize = envs->g_size;
+    double gnear[3*gsize];
+    double gper[3*gsize];
+    double g0[3*gsize];
+    double* gN[3], *gP[3],*gK0[3];
+    
+    for (FINT k = 0; k < 3; ++k){
+        gN[k] = gnear + k*gsize;
+        gP[k] = gper + k*gsize;
+        gK0[k] = g0 + k*gsize;
+    }
+    
     const FINT nmax = envs->li_ceil + envs->lj_ceil;
     const FINT lj = envs->lj_ceil;
     const FINT dj = envs->g_stride_j;
     const double *ri = envs->ri;
     const double *rj = envs->rj;
-    FINT i, j, ptr;
-    double rirk[3], rirj[3];
+    FINT i, j, ptr, k;
+    double rirk[3], rirj[3], ririj[3], rijk[3], ririjk[3];
     double *gx = g;
     double *gy = g + envs->g_size;
     double *gz = g + envs->g_size * 2;
@@ -69,30 +42,73 @@ void CINTg_nuc_exp_ksum(double *g, double* rij, double* cr, double aij, double a
     double aijk = aij + ak;
     double rijrk = 0, dr;
     
-    FINT nk = num_kterms(ak, fac, 1E-9);
-    
     for (FINT k = 0; k < 3; ++k){
+        rijk[k] = ( rij[k]*aij + cr[k]*ak ) / aijk;
         pijk[k] = cr[k] - rij[k];
         rirk[k] = pijk[k] - ri[k];
         rirj[k] = ri[k] - rj[k];
+        ririj[k] = ri[k] - rij[k];
         dr = rij[k] - cr[k];
         rijrk += dr*dr;
     }
+    
+    // gnear and gk=0
+    
+    double Cnear = exp(-aij*ak*rijrk/aijk) / (aijk * sqrt(aijk) ) ;
+    double Ck = PI * PI * PI / (ak * sqrt(ak) * sqrt(aij) * aij );
+    
+    for (k = 0; k < 3; ++k){
+        ririjk[k] = ri[k] - rijk[k];
+        ririj[k] = ri[k] - rij[k];
+    }
+    
+    gN[0][0] = 1;
+    gN[1][0] = 1;
+    gN[2][0] = SQRTPI * PI * fac * Cnear;
+    
+    gK0[0][0] = 1;
+    gK0[1][0] = 1;
+    gK0[2][0] = fac * Ck;
+    
+    if (nmax > 0) {
+        for (k = 0; k < 3; ++k){
+            gN[k][1] = -ririjk[k] * gN[k][0];
+            gK0[k][1] = -ririj[k] * gK0[k][0];
+        }
+    }
+    
+    for (i = 1; i < nmax; i++) {
+        for (k = 0; k < 3; ++k){
+            gN[k][i+1] = 0.5 * i / aij * gN[k][i-1] - ririjk[0] * gN[k][i];
+            gK0[k][i+1] = 0.5 * i / aij * gK0[k][i-1] - ririj[0] * gK0[k][i];
+        }
+    }
+    
+    for (j = 1; j <= lj; j++) {
+        ptr = dj * j;
+        for (i = ptr; i <= ptr + nmax - j; i++) {
+            for (k = 0; k < 3; ++k){
+                gN[k][i] = gN[k][i+1-dj] + rirj[k] * gN[k][i-dj];
+                gK0[k][i] = gK0[k][i+1-dj] + rirj[k] * gK0[k][i-dj];
+            }
+        }
+    }
+    
+    // k sum
+    FINT nk = num_kterms(ak, fac, 1E-9);
     double complex gk[3*envs->g_size];
-    double complex* gk_x = gk;
-    double complex* gk_y = gk + envs->g_size;
-    double complex* gk_z = gk + envs->g_size * 2;
-    double expfac = PI*PI*ak*aij / (ak + aij);
+    double complex* gkx = gk;
+    double complex* gky = gk + envs->g_size;
+    double complex* gkz = gk + envs->g_size * 2;
+    double expfac = PI*PI*aijk/(ak*aij);
     double complex ikpi[3];
     for (i = 0; i < 3*envs->g_size; ++i) g[i] = 0;
     
-    double fac2 = exp(-(aij*ak/aijk)*rijrk)*fac;
-    
     for (FINT k = 0; k < nk; ++k){
         
-        gk_x[0] = exp(-k*k*expfac)*exp(2*I*PI*k*pijk[0]);
-        gk_y[0] = exp(-k*k*expfac)*exp(2*I*PI*k*pijk[1]);
-        gk_z[0] = exp(-k*k*expfac)*exp(2*I*PI*k*pijk[2]);
+        gkx[0] = exp(-k*k*expfac)*exp(2*I*PI*k*pijk[0]);
+        gky[0] = exp(-k*k*expfac)*exp(2*I*PI*k*pijk[1]);
+        gkz[0] = exp(-k*k*expfac)*exp(2*I*PI*k*pijk[2]);
         
         if (nmax > 0) {
             
@@ -100,34 +116,41 @@ void CINTg_nuc_exp_ksum(double *g, double* rij, double* cr, double aij, double a
             ikpi[1] = -I*PI*k/aij + rirk[1];
             ikpi[2] = -I*PI*k/aij + rirk[2];
             
-            gk_x[1] = ikpi[0] * gk_x[0];
-            gk_y[1] = ikpi[1] * gk_y[0];
-            gk_z[1] = ikpi[2] * gk_z[0];
+            gkx[1] = ikpi[0] * gkx[0];
+            gky[1] = ikpi[1] * gky[0];
+            gkz[1] = ikpi[2] * gkz[0];
             
         }
+        
         for (i = 1; i < nmax; i++) {
-            gk_x[i+1] = 0.5 * i / aij * gk_x[i-1] + ikpi[0] * gk_x[i];
-            gk_y[i+1] = 0.5 * i / aij * gk_y[i-1] + ikpi[1] * gk_y[i];
-            gk_z[i+1] = 0.5 * i / aij * gk_z[i-1] + ikpi[2] * gk_z[i];
+            gkx[i+1] = 0.5 * i / aij * gkx[i-1] + ikpi[0] * gkx[i];
+            gky[i+1] = 0.5 * i / aij * gky[i-1] + ikpi[1] * gky[i];
+            gkz[i+1] = 0.5 * i / aij * gkz[i-1] + ikpi[2] * gkz[i];
         }
         for (j = 1; j <= lj; j++) {
             ptr = dj * j;
             for (i = ptr; i <= ptr + nmax - j; i++) {
                 
-                gk_x[i] = gk_x[i+1-dj] + rirj[0] * gk_x[i-dj];
-                gk_y[i] = gk_y[i+1-dj] + rirj[1] * gk_y[i-dj];
-                gk_z[i] = gk_z[i+1-dj] + rirj[2] * gk_z[i-dj];
+                gkx[i] = gkx[i+1-dj] + rirj[0] * gkx[i-dj];
+                gky[i] = gky[i+1-dj] + rirj[1] * gky[i-dj];
+                gkz[i] = gkz[i+1-dj] + rirj[2] * gkz[i-dj];
                 
-                gx[i] += cabs(gk_x[i]);
-                gy[i] += cabs(gk_y[i]);
-                gz[i] += cabs(gk_z[i])*fac2;
+                gx[i] += cabs(gkx[i]);
+                gy[i] += cabs(gky[i]);
+                gz[i] += cabs(gkz[i])*fac * Ck;
             }
         }
         for (i = 0; i <= nmax; i++){
-            gx[i] += cabs(gk_x[i]);
-            gy[i] += cabs(gk_y[i]);
-            gz[i] += cabs(gk_z[i])*fac2;
+            gx[i] += cabs(gkx[i]);
+            gy[i] += cabs(gky[i]);
+            gz[i] += cabs(gkz[i])*fac * Ck;
         }
+    }
+    
+    double *gxyz[3] = {gx, gy, gz};
+    for (i = 0; i < gsize; ++i){
+        for (k = 0; k < 3; ++k)
+        gxyz[k][i] = gN[k][i] - gK0[k][i] + gP[k][i];
     }
 }
 FINT num_kterms(double ak, double wk, double epsilion){
@@ -140,77 +163,4 @@ FINT num_kterms(double ak, double wk, double epsilion){
     }
     return N;
 }
-double JTsum_x0(double* gk_real, double* gk_img, double* R, double* alpha, FINT Nk){
-    double aijk_inv = ( 1.0/alpha[0] + 1.0/alpha[1] )*M_PI*M_PI;
-    double P = R[1] - R[0];
-    double cosT, sinT;
-    FINT k = 0;
-    double out = 1;
-    double fac;
-    for (k = 0; k < Nk; ++k){
-        fac = 2*exp(-k*k*aijk_inv);
-        cosT = cos(2*M_PI*k*P)*fac;
-        sinT = sin(2*M_PI*k+P);
-        gk_real[k] = cosT;
-        gk_img[k] = sinT;
-        out += sqrt(cosT*cosT + sinT*sinT)*fac;
-    }
-    return out;
-}
-double JTsum_x1(double* gk_real, double* gk_img, double* R, double* alpha, FINT Nk){
-    
-    
-}
-double JTsum_x2(double* gk_real, double* gk_img, double* R, double* alpha, FINT Nk){
-    
-}
-double JT_1D_sum(double aij, double ak, double wk, double pij_k, double cr_k){
-    double fac = 1;
-    /*
-    exp [ - k^2 pi^2 / ak ] * integral exp[-aij*(x-Pij)^2 * exp[2*Pi*I*(x-R)]
-    x' = x - R -> x' + R = x'
-     
-     exp [ - k^2 pi^2 / ak ] * integral exp[-aij*(x' + R -Pij )^2 * exp[2*Pi*I*x*k]
-     
-     = exp[- k^2 pi^2 / ak] * ( exp [ - k^2 * pi^2 / aij ] * exp [2*i*p]
-     
-     Ck = exp[ - k^2 pi^2 ( 1.0/ak + 1.0/ aij ] * exp[2*i*p] pi^2 / [sqrt(ak) ]
-     
-     x^j, j = 0 : I00k = Ck / sqrt(aij)
-     
-     */
-    
-    double aijk_inv = ( 1.0/ak + 1.0/aij )*M_PI*M_PI;
-    double P = cr_k - pij_k;
-    double cosT, sinT;
-    FINT k = 0;
-    double out = 1;
-    while (fac > 1E-9){
-        ++k;
-        fac = 2*exp(-k*k*aijk_inv);
-        cosT = cos(2*M_PI*k*P);
-        sinT = sin(2*M_PI*k+P);
-        
-        out += sqrt(cosT*cosT + sinT*sinT)*fac;
-    }
-    return out;
-}
-/*
-    Exp [ - pi^2 * k^2 / aij ] * exp[2*i*p] * (a*(p-r1) - i*pi*w)
-    
-    I_10k = I_00k*(p-r1) - i*pi*k / aij
- 
-    I_20k = I
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- */
+
